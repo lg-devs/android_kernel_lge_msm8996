2040221111b2d88aa0c0015444e7d2b765dbc131
Rashed Abdel-Tawab <rashed@linux.com>
2016-08-26 16:07:43 -0400

    Revert "s390: avoid z13 cache aliasing"
    This reverts commit aff3b5937235209b93fa30cbe112728fa9f8022f.

--- arch/s390/include/asm/elf.h
+++ arch/s390/include/asm/elf.h
@@ -163,8 +163,8 @@ extern unsigned int vdso_enabled;
    the loader.  We need to make sure that it is out of the way of the program
    that it will "exec", and that there is sufficient room for the brk.  */
 
-extern unsigned long randomize_et_dyn(void);
-#define ELF_ET_DYN_BASE		randomize_et_dyn()
+extern unsigned long randomize_et_dyn(unsigned long base);
+#define ELF_ET_DYN_BASE		(randomize_et_dyn(STACK_TOP / 3 * 2))
 
 /* This yields a mask that user programs can use to figure out what
    instruction set this CPU supports. */
@@ -209,9 +209,7 @@ do {								\
 } while (0)
 #endif /* CONFIG_COMPAT */
 
-extern unsigned long mmap_rnd_mask;
-
-#define STACK_RND_MASK	(mmap_rnd_mask)
+#define STACK_RND_MASK	0x7ffUL
 
 #define ARCH_DLINFO							    \
 do {									    \
