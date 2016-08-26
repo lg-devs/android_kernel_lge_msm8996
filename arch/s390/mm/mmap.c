a1d569a83f3f7a0b88b18a680035c8fb61f18b03
Rashed Abdel-Tawab <rashed@linux.com>
2016-08-26 16:07:42 -0400

    Revert "s390/mm: align 64-bit PIE binaries to 4GB"
    This reverts commit 77da131c8ce67fae5c3c9bf7ec23fd6ac7721a02.

--- arch/s390/mm/mmap.c
+++ arch/s390/mm/mmap.c
@@ -183,10 +183,7 @@ unsigned long randomize_et_dyn(void)
 {
 	unsigned long base;
 
-	base = STACK_TOP / 3 * 2;
-	if (!is_32bit_task())
-		/* Align to 4GB */
-		base &= ~((1UL << 32) - 1);
+	base = (STACK_TOP / 3 * 2) & (~mmap_align_mask << PAGE_SHIFT);
 	return base + mmap_rnd();
 }
 
